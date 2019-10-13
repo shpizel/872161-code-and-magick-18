5d6667b6268ec770f1c689557cb8b799bd5854f1
Igor Shpizel <igor@shpizel.ru>
2019-10-13 20:02:35 +0300

    fixes

--- js/setup.js
+++ js/setup.js
@@ -1,25 +1,27 @@
 'use strict';
 
-var NAMES = 'Иван, Хуан Себастьян, Мария, Кристоф, Виктор, Юлия, Люпита, Вашингтон'.split(',');
-var SURNAMES = 'да Марья, Верон, Мирабелла, Вальц, Онопко, Топольницкая, Нионго, Ирвинг'.split(',');
-var COAT_COLORS = 'rgb(101, 137, 164); rgb(241, 43, 107); rgb(146, 100, 161); rgb(56, 159, 117); rgb(215, 210, 55); rgb(0, 0, 0)'.split(';');
-var EYES_COLORS = 'black, red, blue, yellow, green'.split(',');
+var NAMES = ['Иван', 'Хуан', 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
-var random = function (min, max) {
+var QUANTITY = 4;
+
+var getRandomNumber = function (min, max) {
   return min + Math.floor(Math.random() * (max - min + 1));
 };
 
 var getRandomElement = function (array) {
-  return array[random(0, array.length - 1)].trim();
+  return array[getRandomNumber(0, array.length - 1)].trim();
 };
 
 var getRandomName = function () {
-  var reversed = random(0, 1);
+  var reversed = getRandomNumber(0, 1);
   var ret = getRandomElement(NAMES);
   if (reversed) {
-    ret = getRandomElement(SURNAMES) + ' ' + ret;
+    ret = getRandomElement(SURNAMES) + ' ' + ret;
   } else {
-    ret += ' ' + getRandomElement(SURNAMES);
+    ret += ' ' + getRandomElement(SURNAMES);
   }
   return ret;
 };
@@ -32,25 +34,32 @@ var getRandomCharacter = function () {
   };
 };
 
+var genearateRandomCharacters = function (quantity) {
+  var characters = [];
+  for (var i = 0; i < quantity; i++) {
+    characters.push(getRandomCharacter());
+  }
+  return characters;
+};
+
+var getDOMElementFromCharacter = function (character) {
+  var template = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+  var element = template.cloneNode(true);
+  element.querySelector('.setup-similar-label').textContent = character.name;
+  element.querySelector('.wizard-coat').style.fill = character.coatColor;
+  element.querySelector('.wizard-eyes').style.fill = character.eyesColor;
+  return element;
+}
+
+var fillSimilarList = function (quantity) {
+  var fragment = document.createDocumentFragment();
+  var characters = genearateRandomCharacters(quantity);
+  for (var i = 0; i < characters.length; i++) {
+    fragment.appendChild(getDOMElementFromCharacter(characters[i]));
+  }
+  document.querySelector('.setup-similar-list').appendChild(fragment);
+  document.querySelector('.setup-similar').classList.remove('hidden');
+};
+
+fillSimilarList(QUANTITY);
 document.querySelector('.setup').classList.remove('hidden');
-
-var characters = [];
-for (var i = 0; i < 4; i++) {
-  characters.push(getRandomCharacter());
-}
-
-var template = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-var fragment = document.createDocumentFragment();
-
-for (var c = 0; c < characters.length; c++) {
-  var cloned = template.cloneNode(true);
-
-  cloned.querySelector('.setup-similar-label').textContent = characters[c].name;
-  cloned.querySelector('.wizard-coat').style.fill = characters[c].coatColor;
-  cloned.querySelector('.wizard-eyes').style.fill = characters[c].eyesColor;
-
-  fragment.appendChild(cloned);
-}
-
-document.querySelector('.setup-similar-list').appendChild(fragment);
-document.querySelector('.setup-similar').classList.remove('hidden');
